9914b914e8af62b19f88b36504f2af61caeaec1d
pranitha-369 <89985595+pranitha-369@users.noreply.github.com>
2023-04-18 21:58:05 +0530

    new commit

---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+@app.route('/index/')
+def hello():
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/about/')
+def about():
+    return '<h3>This is a Flask web application.</h3>'
